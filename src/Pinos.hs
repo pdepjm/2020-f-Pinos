9c5b340264d3e81d5e9c88d750e6b023ccfcd80b
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2020-04-16 12:14:42 -0300

    agregando solucion final y mas tests

--- src/Pinos.hs
+++ src/Pinos.hs
@@ -36,6 +36,7 @@ limite = 3
 -- Punto 2
 -- Definí la función esPesoUtil, recibe un peso en kg y responde si un pino de ese peso le sirve a la fábrica
 -- Los pinos se usan para llevarlos a una fábrica de muebles, a la que le sirven árboles de entre 400 y 1000 kilos, un pino fuera de este rango no le sirve a la fábrica.
+esPesoUtil peso = 400 <= peso && peso <= 1000
 
 -- Punto 3
 -- Definí la función sirvePino, 
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,7 @@ import Pinos
 
 main :: IO()
 main = hspec $ do
- describe "Tests de Peso Pino" $ do
+  describe "Tests de Peso Pino" $ do
      it "El peso de un pino petiso se calcula bien" $ do
        pesoPino 2 `shouldBe` 600
      it "El peso de un pino alto se calcula bien" $ do
@@ -11,20 +11,7 @@ main = hspec $ do
   describe "Tests de sirve  Pino" $ do
       it "Un pino mediano sirve" $ do
         sirvePino 2 `shouldBe` True
-
-
-
-
-
-
-
-
-
-{-
-     it "El peso de un pino alto se calcula bien" $ do
-       pesoPino 4 `shouldBe` 1100
-  describe "Tests de Peso Pino" $ do
-     it "El peso de un pino petiso se calcula bien" $ do
-       pesoPino 2 `shouldBe` 600
-
--}
+      it "Un pino chiquito no sirve" $ do
+        sirvePino 1 `shouldBe` False
+      it "Un pino gigante no sirve" $ do
+        sirvePino 5 `shouldBe` False
